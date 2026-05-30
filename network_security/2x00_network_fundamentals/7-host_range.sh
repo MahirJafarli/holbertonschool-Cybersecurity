00131551ec65130e3509cc46534362dd52810dc5
MahirJafarli <jafarlimahir@gmail.com>
2026-05-30 19:18:53 +0000

    Create Task 7

---
+++ network_security/2x00_network_fundamentals/7-host_range.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+printf "%d.%d.%d.%d - %d.%d.%d.%d" $(IFS=.; ip=($1); m=(); for i in 1 2 3 4; do m+=($(( $2 >= i*8 ? 255 : ($2 <= (i-1)*8 ? 0 : 256 - (1 << (8 - ($2 % 8))) ) ))); done; for i in {0..3}; do echo $((${ip[$i]} & ${m[$i]} + (i==3))); done; for i in {0..3}; do echo $((${ip[$i]} | (255 ^ ${m[$i]}) - (i==3))); done)
