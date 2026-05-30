05becda8429a3964efd4fcd2800ebdad15db7ee5
MahirJafarli <jafarlimahir@gmail.com>
2026-05-30 19:13:19 +0000

    Fix

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf "%d.%d.%d.%d\n" $(($((0xffffffff << (32 - $1))) >> 24 & 0xff)) $(($((0xffffffff << (32 - $1))) >> 16 & 0xff)) $(($((0xffffffff << (32 - $1))) >> 8 & 0xff)) $(($((0xffffffff << (32 - $1))) & 0xff))
+IFS=.; printf "%d.%d.%d.%d\n" $(for i in {1..4}; do bc <<< "ibase=2; $([ $1 -ge $((i*8)) ] && echo 11111111 || ([ $1 -le $(((i-1)*8)) ] && echo 00000000 || printf '1%.0s' $(seq 1 $(($1 % 8))); printf '0%.0s' $(seq 1 $((8 - ($1 % 8)))); echo))"); done)
